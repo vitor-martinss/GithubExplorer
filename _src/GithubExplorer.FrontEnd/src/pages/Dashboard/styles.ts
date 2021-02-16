3e6db3e56e8ae224b750efa278d138e302e67511
Vitor Martins <vitormartinsweb@gmail.com>
2021-02-16 19:22:56 +0000

    UPD styling dashboard mobile version

--- _src/GithubExplorer.FrontEnd/src/pages/Dashboard/styles.ts
+++ _src/GithubExplorer.FrontEnd/src/pages/Dashboard/styles.ts
@@ -9,12 +9,16 @@ interface FormProps {
 
 // Template literals
 export const Title = styled.h1`
-	font-size: 4.8rem;
+	font-size: 3rem;
 	color: #3a3a3a;
-	max-width: 45rem;
-	line-height: 5.6rem;
-
+	line-height: 3.6rem;
 	margin-top: 8rem;
+
+	@media (min-width: 768px) {
+		font-size: 4.8rem;
+		line-height: 5.6rem;
+		max-width: 45rem;
+	}
 `;
 
 // eslint-disable-next-line prettier/prettier
@@ -23,16 +27,25 @@ export const Form = styled.form<FormProps>`
 	max-width: 70rem;
 
 	display: flex;
+	flex-wrap: wrap;
 
+	@media (min-width: 768px) {
+		flex-wrap: nowrap;
+	}
 	input {
 		flex: 1;
+		width: 100%;
 		height: 7rem;
 		padding: 0 2.4rem;
 		border: 0;
 		border-radius: 0.5rem 0 0 0.5rem;
 		color: #a3a3a3;
 		border: .2rem solid #fff;
-		border-right: 0;
+
+		@media (min-width: 768px) {
+			width: auto;
+			border-right: 0;
+		}
 
 		${props => props.hasError && css`
 				border-color: #c53030;
@@ -44,14 +57,19 @@ export const Form = styled.form<FormProps>`
 	}
 
 	button {
-		width: 21rem;
+		width: 100%;
 		height: 7rem;
 		background: #04d361;
-		border-radius: 0 0.5rem 0.5rem 0;
+		border-radius: 0.5rem;
 		border: 0;
 		color: #fff;
 		font-weight: bold;
 		transition: background-color 0.2s;
+
+		@media (min-width: 768px) {
+			max-width: 21rem;
+			border-radius: 0 0.5rem 0.5rem 0;
+		}
 
 		&:hover {
 			background: ${shade(0.2, '#04d361')};
@@ -73,8 +91,12 @@ export const Repositories = styled.div`
 		background: #fff;
 		border-radius: 0.5rem;
 		width: 100%;
-		padding: 2.4rem;
+		padding: 1rem;
 		text-decoration: none;
+
+		@media (min-width: 768px) {
+			padding: 2.4rem;
+		}
 
 		display: flex;
 		align-items: center;
@@ -89,23 +111,36 @@ export const Repositories = styled.div`
 		}
 
 		img {
-			width: 6.4rem;
-			height: 6.4rem;
+			width: 0;
+			height: 0;
 			border-radius: 50%;
+
+			@media (min-width: 768px) {
+				width: 6.4rem;
+				height: 6.4rem;
+			}
 		}
 
 		div {
 			margin: 0 1.6rem;
 			flex: 1;
 			strong {
-				font-size: 2rem;
+				font-size: 1.6rem;
 				color: #3d3d4d;
+
+				@media (min-width: 768px) {
+					font-size: 2rem;
+				}
 			}
 
 			p {
-				font-size: 1.8rem;
+				font-size: 1.4rem;
 				color: #a8a8b3;
 				margin-top: 0.4rem;
+
+				@media (min-width: 768px) {
+					font-size: 1.8rem;
+				}
 			}
 		}
 
